7ac110e79867e61e6424f07462639f44af0c280b
Michael Joseph <michaeljoseph+github@gmail.com>
2014-12-08 16:01:25 +0200

    0.2.2

--- pyconza2014/__init__.py
+++ pyconza2014/__init__.py
@@ -3,7 +3,7 @@
 __author__ = 'Michael Joseph'
 __email__ = 'michaeljoseph@gmail.com'
 __url__ = 'https://github.com/michaeljoseph/pyconza2014'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 
 def pyconza2014():
